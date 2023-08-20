de9169149cfe9d29157f180b027d658c77410191
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-08-20 14:34:38 +0300

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [num for num in num_list if num % 2 == 0]
+    return even_numbers
+    
+num_list = [1,2,3,4,5,6,78,10,90]
+result = return_evens(num_list)
+print(result)   
+       
 
 def make_exclamation(sentence_list):
-    pass
+    return_exclamation = [sentence + "!" for sentence in sentence_list]
+    return return_exclamation
+
+sentence_list = ["How are you"]
+result = make_exclamation(sentence_list)
+print(result)    
